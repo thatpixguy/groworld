23ee8a6419e3a1ee2a5d53948a12c82484b06e6c
pix <thatpixguy@gmail.com>
2010-11-17 12:15:52 +1030

    fresh rails3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groworld::Application.config.secret_token = '81f9383d3c06dd631c42e794809e656cb148eeebcf36063917eb02f6ba863e074c3a8c718e163d7cf336fce27c665a395a29b5431e55ebd8b69d26ebd0426154'
